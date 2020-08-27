8b60504a0a966fdeb113a86e1fa27e2dab671e12
Dana Wilson <danalwil@outlook.com>
2020-08-27 14:08:51 -0400

    10th Commit: Section Test

--- SololearnCSharp/Program.cs
+++ SololearnCSharp/Program.cs
@@ -14,25 +14,18 @@ namespace SololearnCSharp
             char y = 'Z';
             bool isOnline = true;
             string firstName = "David";
-
             Console.WriteLine(x);
-
             string yourName;
             Console.WriteLine("What is your name?");
             yourName = Console.ReadLine();
             Console.WriteLine($"Hello {yourName}.");
-
             //Comment
-
             var num = 42;
-
             const double Pi = 3.14;
-
             int t = 8;
             int d = 15;
             Console.WriteLine(d - t);
             //Outputs 7
-
             int v = 42;
             v + 2;
             v -= 6;

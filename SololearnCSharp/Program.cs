0ed67a35765488e3b667e9bb9456511bba9ecb82
Dana Wilson <danalwil@outlook.com>
2020-08-27 13:35:16 -0400

    Add project files.

---
+++ SololearnCSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SololearnCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int myAge;
+            myAge = 18;
+
+        }
+    }
+}
